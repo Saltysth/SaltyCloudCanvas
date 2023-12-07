0505102ea04de1ecd73c73eac32b5464a8bdaabc
Saltysth <1432488520@qq.com>
2023-12-07 13:32:18 +0800

    init

---
+++ src/main/java/com/salty/canvas/SaltyCloudCanvasApplication.java
@@ -0,0 +1,13 @@
+package com.salty.canvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltyCloudCanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltyCloudCanvasApplication.class, args);
+	}
+
+}
