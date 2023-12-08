0ad942ca9f179dea2a8c1a7151756c9915d861e8
Saltysth <1432488520@qq.com>
2023-12-08 18:22:15 +0800

    Revert "去掉不需要的部分，并且修复监控不全的问题"
    This reverts commit dbd5ba945113bf832b1dd32385e5cbc3d95c9a19.

---
+++ src/test/java/com/salty/canvas/SaltyCloudCanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salty.canvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltyCloudCanvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
